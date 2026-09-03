eed2574ab9478b5d7a7476dcaf17c1f4fbcfd10e
soleildayana <soleil.nino@udea.edu.co>
2026-09-02 22:39:58 -0500

    Completar scriptsd de py y creación de pipeline

--- constructor_db.py
+++ constructor_db.py
@@ -1,19 +1,23 @@
 # LIBRERIAS
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import sqlite3
 
-# Accedemos a la base de datos para leer el csv crudo
+# Descarga SQL remota desde SDSS DR18
 url = 'https://skyserver.sdss.org/dr18/SkyServerWS/SearchTools/SqlSearch?format=csv&cmd=SELECT%20TOP%205000%20s.class%2C%20s.z%2C%20p.psfMag_g%20as%20g%2C%20p.psfMag_r%20as%20r%20FROM%20PhotoObj%20AS%20p%20JOIN%20SpecObj%20AS%20s%20ON%20p.objid%20%3D%20s.bestobjid'
-data = pd.read_csv(url, skiprows=[0]) # Se omite la primera fila ya que es el nombre de la tabla. Si no se hace este paso, la tabla queda como una única columna de nombre '#Table1'
+data = pd.read_csv(url, skiprows=[0])
 
-# Eliminar objetos de clase STAR
-for index, row in data.iterrows():
-    if row['class'] == 'STAR':
-        data.drop(index, inplace=True)
+# Eliminar objetos de clase STAR (no son galaxias ni quásares)
+data = data[data['class'] != 'STAR'].reset_index(drop=True)
 
-# Calcular el índice g - r
+# Calcular índice de color g - r
 data['g_r'] = data['g'] - data['r']
 
-#  Pendiente de guardar los datos procesados en base de datos local SQLite
+print(f"\nDistribución por clase:")
+print(data['class'].value_counts())
+
+# Migrar a SQLite local
+conexion = sqlite3.connect('datos_mision.db')
+data.to_sql('objetos', conexion, if_exists='replace', index=True)
+conexion.close()
+
+print("\n✓ Base de datos 'datos_mision.db' creada exitosamente.")
